7dd5f5221ab63d18f761a66d46288fdba8e5b0b7
Mark Sauer-Utley <mark.utley.713@gmail.com>
2018-03-27 11:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + parseInt(two)
 }
 
 var funkyFunction = function() {
